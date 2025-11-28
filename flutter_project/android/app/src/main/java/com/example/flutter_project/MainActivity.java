62d20d5c9d9cf12abb9101a9239abfae7dba0cde
roma0323 <r911223oger@gmail.com>
2025-11-28 15:58:46 +0800

    fix: third times fix

--- flutter_project/android/app/src/main/java/com/example/flutter_project/MainActivity.java
+++ flutter_project/android/app/src/main/java/com/example/flutter_project/MainActivity.java
@@ -1,27 +1,25 @@
 package com.example.flutter_project;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import android.util.Log;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.security.ProviderInstaller;
-import android.os.Bundle;
-import android.util.Log;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Update security provider to protect against SSL exploits
+        updateAndroidSecurityProvider();
+    }
+
+    private void updateAndroidSecurityProvider() {
         try {
-            ProviderInstaller.installIfNeeded(getApplicationContext());
-            Log.d(TAG, "Security provider updated successfully");
-        } catch (GooglePlayServicesRepairableException e) {
-            Log.e(TAG, "Google Play Services Repairable Exception", e);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Log.e(TAG, "Google Play Services Not Available", e);
+            ProviderInstaller.installIfNeeded(this);
+            Log.d("Security", "Google Play Services security provider updated successfully.");
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            Log.e("Security", "Google Play Services is not available or needs repair.", e);
         }
     }
 }
